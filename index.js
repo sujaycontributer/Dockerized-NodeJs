9653400579a32810cb3ade32f013f8c10a5b6490
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-07-18 22:22:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hi there, running from the docker container optimized way"); // simple nodejs with docker
+    res.send("Hi there, running from the docker container optimized way"); r
 })
 
 app.listen(3000);
